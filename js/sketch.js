cd2b593ab3eb68372b448d6d4ce33a8c8def387c
lunacoppin <luna.coppin@student.howest.be>
2025-06-16 14:12:32 +0200

    different visual (blob) that matches our concept

--- js/sketch.js
+++ js/sketch.js
@@ -1,44 +1,75 @@
-let img;
-let emitters = [];
+const blobs = {};
 
-function preload() {
-  img = loadImage('./img/texture32.png'); // flame orb image
+function createBlob(id) {
+  const el = document.createElement('div');
+  el.className = 'gradients-container';
+  el.id = `blob-${id}`;
+  el.innerHTML = `
+    <div class="g1"></div>
+    <div class="g2"></div>
+    <div class="g3"></div>
+    <div class="g4"></div>
+    <div class="g5"></div>
+    <div class="g6"></div>
+  `;
+  document.getElementById('gradient-bg').appendChild(el);
+  return el;
 }
 
-function setup() {
-  const canvasElement = document.querySelector('.app_p5js-canvas');
-
-  // Use p5.js with existing canvas element
-  let canvas = createCanvas(canvasElement.clientWidth, canvasElement.clientHeight);
-  canvas.elt = canvasElement; // attach the p5 renderer to the existing canvas
-  canvas.drawingContext = canvasElement.getContext('2d');
-
-  blendMode(ADD);
-}
-
-function draw() {
-  clear();
-  background(0, 0, 0, 30);
-
+function updateBlobPositions() {
   const poses = typeof getLatestPoses === 'function' ? getLatestPoses() : [];
 
-  // Ensure emitter count matches tracked people
-  while (emitters.length < poses.length) {
-    emitters.push(new Emitter(width / 2, height / 2));
-  }
+  // Cleanup: Remove blobs for undetected people
+  Object.keys(blobs).forEach(id => {
+    if (!poses[id]) {
+      blobs[id].el.remove();
+      delete blobs[id];
+    }
+  });
 
-  for (let i = 0; i < poses.length; i++) {
-    const pose = poses[i];
-    const nose = tracker.findKeypoint('nose', pose);
-    if (nose && nose.score > tracker.minScore) {
-      const x = tracker.scaleX(nose.x);
-      const y = tracker.scaleY(nose.y);
-      emitters[i].setPosition(x, y);
+  poses.forEach((pose, i) => {
+    const id = i.toString();
+    if (!blobs[id]) {
+      blobs[id] = {
+        el: createBlob(id),
+        x: 0,
+        y: 0,
+        prevX: 0,
+        prevY: 0
+      };
     }
 
-    emitters[i].emit(2);
-    emitters[i].applyForce(createVector(0, -0.02));
-    emitters[i].update();
-    emitters[i].show();
-  }
+    const nose = tracker.findKeypoint('nose', pose);
+
+    if (nose && nose.score > tracker.minScore) {
+      const blob = blobs[id];
+
+      const targetX = tracker.scaleX(nose.x);
+      const targetY = tracker.scaleY(nose.y);
+
+      blob.x += (targetX - blob.x) * 1;
+      blob.y += (targetY - blob.y) * 1;
+
+      const dx = blob.x - blob.prevX;
+      const dy = blob.y - blob.prevY;
+      blob.prevX = blob.x;
+      blob.prevY = blob.y;
+
+      const stretchX = 1 + Math.min(Math.abs(dx) / 50, 0.5);
+      const stretchY = 1 + Math.min(Math.abs(dy) / 50, 0.5);
+
+      blob.el.style.transform = `
+        translate(${2.5* blob.x}px, ${2.5 * blob.y}px)
+        scale(${stretchX}, ${stretchY})
+      `;
+    }
+  });
 }
+
+
+function loop() {
+  updateBlobPositions();
+  requestAnimationFrame(loop);
+}
+
+loop();
